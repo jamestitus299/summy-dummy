89b95bc1f21ff60ec284ad2203ce15d1ac680469
James Titus <james@alphanome.ai>
2025-03-07 05:19:45 +0000

    update: lucid-react

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as CheckReactCode } from './components/CheckCodeRender';
-export { default as ReactCanvas } from './components/ReactCanvas';
-export type { CheckCodeRenderProps } from './components/CheckCodeRender';
-export type { ReactCanvasProps } from './components/ReactCanvas';
-export { default as JamButton } from './components/ButtonText';

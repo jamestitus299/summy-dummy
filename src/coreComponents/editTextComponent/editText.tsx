97713c8d5d7030c9ea228a8ef6e82165a13f852c
James Titus <james@alphanome.ai>
2025-11-21 15:27:37 +0530

    chore: export edittextcompoent

--- src/coreComponents/editTextComponent/editText.tsx
+++ src/coreComponents/editTextComponent/editText.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 
-interface EditableTextProps {
+export interface EditableTextProps {
   textContent?: string;
   elementType?: keyof JSX.IntrinsicElements;
   placeholder?: string;

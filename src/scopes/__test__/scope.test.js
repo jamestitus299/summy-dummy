57b1e694db3ed823efdf0dbdc922cce1104b6c19
James Titus <james@alphanome.ai>
2025-11-25 15:40:17 +0530

    chore: added test for scope

---
+++ src/scopes/__test__/scope.test.js
@@ -0,0 +1,49 @@
+import { scope } from "../Scope";
+
+// Test Scope
+describe("scope object", () => {
+    test("contains React and hooks", () => {
+        expect(scope.React).toBeDefined();
+        expect(scope.useState).toBeDefined();
+        expect(scope.useEffect).toBeDefined();
+        expect(scope.useContext).toBeDefined();
+        expect(scope.useReducer).toBeDefined();
+        expect(scope.useRef).toBeDefined();
+        expect(scope.useMemo).toBeDefined();
+        expect(scope.useCallback).toBeDefined();
+    });
+
+    test("contains motion and motion hooks/components", () => {
+        expect(scope.motion).toBeDefined();
+        expect(scope.useAnimate).toBeDefined();
+        expect(scope.useScroll).toBeDefined();
+        expect(scope.LazyMotion).toBeDefined();
+        expect(scope.useReducedMotion).toBeDefined();
+    });
+
+    test("contains recharts components", () => {
+        expect(scope.AreaChart).toBeDefined();
+        expect(scope.ResponsiveContainer).toBeDefined();
+        expect(scope.CartesianGrid).toBeDefined();
+        expect(scope.LineChart).toBeDefined();
+        expect(scope.XAxis).toBeDefined();
+    });
+
+    test("contains lucid react components", () => {
+        expect(scope.Camera).toBeDefined();
+        expect(scope.StarHalf).toBeDefined();
+        expect(scope.Smile).toBeDefined();
+        expect(scope.Landmark).toBeDefined();
+        expect(scope.House).toBeDefined();
+    });
+
+    test("contains lucid react-icons/fa components", () => {
+        expect(scope.FaBeer).toBeDefined();
+        expect(scope.FaHeart).toBeDefined();
+    });
+
+    test("contains EditableText custom components", () => {
+        expect(scope.EditableText).toBeDefined();
+    });
+
+});
